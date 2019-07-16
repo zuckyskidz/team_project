689b8896243a9cc32cdc148d77c9386c60ef72c7
jennhe <jenhe2000@gmail.com>
2019-07-16 13:34:23 -0700

    created files and initial xml layout

---
+++ app/src/main/java/com/example/teamproject/SIgnupActivity.java
@@ -0,0 +1,13 @@
+package com.example.teamproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SIgnupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
