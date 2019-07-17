390b01dd7cf7b46d70d27533ea60fe3d347898ce
Catarina Dematos <catdematos@fb.com>
2019-07-17 11:40:25 -0700

    navigate to details page from list Feed

--- app/src/main/java/com/example/teamproject/ListFragment.java
+++ app/src/main/java/com/example/teamproject/ListFragment.java
@@ -1,28 +1,47 @@
 package com.example.teamproject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class ListFragment extends Fragment {
+
+    TextView eventTV;
 
     public ListFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
+
+        eventTV = view.findViewById(R.id.tvTitle);
+        eventTV.setText("Title HERE");
+        eventTV.setClickable(true);
+        eventTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), DetailActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false);
     }
