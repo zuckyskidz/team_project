c13e6c95f5a3c055c65b189b676aa3f8727ca4ab
Catarina Dematos <catdematos@fb.com>
2019-07-24 15:24:00 -0700

    added date and time picker

--- app/src/main/java/com/example/teamproject/CreatePostActivity.java
+++ app/src/main/java/com/example/teamproject/CreatePostActivity.java
@@ -1,12 +1,24 @@
 package com.example.teamproject;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.teamproject.models.Ad;
@@ -14,20 +26,44 @@ import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import org.w3c.dom.Text;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import static com.parse.Parse.getApplicationContext;
 
 public class CreatePostActivity extends AppCompatActivity {
 
+    private static final String TAG = "CreatePostActivity";
     EditText etAdName;
     CalendarView cvAdDate;
+    TextView tvDisplayDate;
+    TextView tvDisplayTime;
     EditText etAdStartTime;
     EditText etAdEndTime;
     EditText etAdAddress;
     EditText etAdDesc;
+
+    final Calendar myCalendar = Calendar.getInstance();
+
+
+    final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateDateLabel();
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +71,37 @@ public class CreatePostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_post);
 
         etAdName = (EditText) findViewById(R.id.etAdName);
-        cvAdDate = (CalendarView) findViewById(R.id.cvAdDate);
         etAdStartTime = (EditText) findViewById(R.id.etAdStartTime);
         etAdEndTime = (EditText) findViewById(R.id.etEndTime);
         etAdAddress = (EditText) findViewById(R.id.etAdAddress);
         etAdDesc = (EditText) findViewById(R.id.etAdDesc);
+        tvDisplayDate = (TextView) findViewById(R.id.tvDateDisplay);
+        tvDisplayTime = (TextView) findViewById(R.id.tvTimeDisplay);
 
-        Date currentTime = Calendar.getInstance().getTime();
-        cvAdDate.setDate(currentTime.getTime());
+        tvDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(CreatePostActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        tvDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(CreatePostActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        myCalendar.set(Calendar.HOUR, selectedHour);
+                        myCalendar.set(Calendar.MINUTE, selectedMinute);
+                        updateTimeLabel();
+                    }
+                }, 12, 00, false);
+                mTimePicker.show();
+            }
+        });
 
 
     }
@@ -50,9 +109,10 @@ public class CreatePostActivity extends AppCompatActivity {
     public void submitAd(View view) {
         Log.d("CreatePostActivity", "Posting...");
         Ad newAd = new Ad();
+
         newAd.setUser(ParseUser.getCurrentUser());
         newAd.setTitle(etAdName.getText().toString());
-        newAd.setDate(new Date(cvAdDate.getDate() * 1000));
+        newAd.setDate(myCalendar.getTime());
         newAd.setStartTime(etAdStartTime.getText().toString());
         newAd.setEndTime(etAdEndTime.getText().toString());
         newAd.setAddress(etAdAddress.getText().toString());
@@ -76,4 +136,17 @@ public class CreatePostActivity extends AppCompatActivity {
         });
     }
 
+    private void updateDateLabel() {
+        String myFormatDate = "EEE, d MMM yyyy"; //In which you need put here
+        SimpleDateFormat sdfDATE = new SimpleDateFormat(myFormatDate, Locale.US);
+
+        tvDisplayDate.setText("Date: " + sdfDATE.format(myCalendar.getTime()));
+    }
+
+    private void updateTimeLabel() {
+        String myFormatTime = "h:mm a"; //In which you need put here
+        SimpleDateFormat sdfTime = new SimpleDateFormat(myFormatTime, Locale.US);
+;
+        tvDisplayTime.setText(String.format("Time: " + sdfTime.format(myCalendar.getTime())));
+    }
 }
